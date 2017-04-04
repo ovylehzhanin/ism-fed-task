31c07fe9c355c557ed453fa9f06697ca0c1fb15f
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2017-04-04 23:39:06 +0300

    Add some styles for mobile header and aside block's

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
 	
-	$('.search-box').slideDown(0);
-	$('.header-top').slideDown(0);
-	$('.header-bottom').slideDown(0);
+	// $('.search-box').slideDown(0);
+	// $('.header-top').slideDown(0);
+	// $('.header-bottom').slideDown(0);
 
-	var navOpener = $('.nav-opener');
+	// var navOpener = $('.nav-opener');
 
-	navOpener.click(function() {
-		$('.search-box').slideToggle('fast');
-		$('.header-top').slideToggle('fast');
-		$('.header-bottom').slideToggle('fast');
-	});
+	// navOpener.click(function() {
+	// 	$('.search-box').slideToggle('fast');
+	// 	$('.header-top').slideToggle('fast');
+	// 	$('.header-bottom').slideToggle('fast');
+	// });
 
-	$('.expander').click(function() {
-		$(this).nextAll().slideToggle('fast');
-	});
+	// $('.expander').click(function() {
+	// 	$(this).nextAll().slideToggle('fast');
+	// });
+
+	
 
 });
