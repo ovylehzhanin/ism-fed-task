2508ef7e0059a2def3fa7de25290924c63a2dc40
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2017-04-04 09:35:52 +0300

    Replace changes to separate branch

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 	
-// 	$('.search-group').slideUp(0);
-// 	$('.header-top').slideUp(0);
-// 	$('.header-bottom').slideUp(0);
+	$('.search-box').slideDown(0);
+	$('.header-top').slideDown(0);
+	$('.header-bottom').slideDown(0);
 
-// 	var navOpener = $('.nav-opener');
+	var navOpener = $('.nav-opener');
 
-// 	navOpener.click(function() {
-// 		$('.search-group').slideToggle('fast');
-// 		$('.header-top').slideToggle('fast');
-// 		$('.header-bottom').slideToggle('fast');
-// 	});
+	navOpener.click(function() {
+		$('.search-box').slideToggle('fast');
+		$('.header-top').slideToggle('fast');
+		$('.header-bottom').slideToggle('fast');
+	});
 
-// 	$('.expander').click(function() {
-// 		$(this).nextAll().slideToggle('fast');
-// 	});
+	$('.expander').click(function() {
+		$(this).nextAll().slideToggle('fast');
+	});
 
-// });
+});
