bb27a87fd60f89970d714ac3c56e8c167d0f594d
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2017-04-19 23:09:32 +0300

    Comment JavaScript code

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,41 @@
 $(document).ready(function() {
 	
-	if ( $(window).width() < 640 ) {
-		$('.mobile-item').hide();
-		$('.list-expander').nextAll().hide();
-	}
+	// if ( $(window).width() < 640 ) {
+	// 	$('.mobile-item').hide();
+	// 	$('.list-expander').nextAll().hide();
+	// }
 
-	$('.menu-opener').click(function() {
-		// e.preventDefault();
+	// $('.menu-opener').click(function() {
+	// 	// e.preventDefault();
 
-		$('.mobile-item').toggleClass('expanded');
+	// 	$('.mobile-item').toggleClass('expanded');
 
-		if ( $(window).width() < 640 ) {
-			$('.mobile-item').slideToggle('fast');
-		}
+	// 	if ( $(window).width() < 640 ) {
+	// 		$('.mobile-item').slideToggle('fast');
+	// 	}
 
-		if ( $(window).width() >= 640 ) {
-			$('.main-menu').toggleClass('visible');
-		}
+	// 	if ( $(window).width() >= 640 ) {
+	// 		$('.main-menu').toggleClass('visible');
+	// 	}
 
-	});
+	// });
 
-	$('.list-expander').click(function() {
+	// $('.filter-opener').click(function() {
 
-		if ( $(window).width() >= 640 ) {
-			if ( !$(this).parent().hasClass('filter-box') ) {
-				return false;
-			}
-		}
+	// 	$('.left-column').slideToggle('fast');
 
-		$(this).nextAll().slideToggle('fast');
-		$(this).parent().toggleClass('expanded-list');
-	});
+	// });
 
-	$(window).resize(function() {
+	// $(window).resize(function() {
 		
-		if ( !$('.mobile-item').hasClass('expanded') ) {
-			$('.mobile-item').slideUp(0);
-		}
+	// 	if ( !$('.mobile-item').hasClass('expanded') ) {
+	// 		$('.mobile-item').slideUp(0);
+	// 	}
 
-		if ($(window).width() > 640) {
-		    $('.mobile-item').css('display','');
-		    $('.list-expander').nextAll().css('display', '');
-		}
-	});
+	// 	if ($(window).width() > 640) {
+	// 	    $('.mobile-item').css('display','');
+	// 	    $('.list-expander').nextAll().css('display', '');
+	// 	}
+	// });
 
 });
